8e63a5c6e078c54365a85bc5e1f5124517f4dae2
amangeldinuraliev-lab <amangeldinuraliev@gmail.com>
2026-08-06 16:28:49 +0600

    Update label.jsx

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,19 +1,19 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef(({ className, type, ...props }, ref) => {
+const Label = React.forwardRef(({ className, ...props }, ref) => {
   return (
-    (<input
-      type={type}
+    <label
+      ref={ref}
       className={cn(
-        "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
         className
       )}
-      ref={ref}
-      {...props} />)
+      {...props}
+    />
   );
 })
-Input.displayName = "Input"
+Label.displayName = "Label"
 
-export { Input }
+export { Label }
+export default Label
